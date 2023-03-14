f130fa34e6ecc5c80ad7933c794da0b2abfb337a
TaksimIslam3847 <islan.taxim@gmail.com>
2023-03-14 17:06:02 -0400

    Initial commit

---
+++ TaksimModule6/src/main/java/taksim/islam/lab6/IslamActivity6.java
@@ -0,0 +1,14 @@
+package taksim.islam.lab6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IslamActivity6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
