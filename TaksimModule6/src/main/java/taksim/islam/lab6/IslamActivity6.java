65ae1a90c5ff72ba1dcad46b361a24e3e3f66f04
TaksimIslam3847 <islan.taxim@gmail.com>
2023-03-16 15:29:13 -0400

    Completed all geekforgeek steps

--- TaksimModule6/src/main/java/taksim/islam/lab6/IslamActivity6.java
+++ TaksimModule6/src/main/java/taksim/islam/lab6/IslamActivity6.java
@@ -1,14 +1,47 @@
 package taksim.islam.lab6;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class IslamActivity6 extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class IslamActivity6 extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.person);
+
+    }
+    FirstFragment firstFragment = new FirstFragment();
+    SecondFragment secondFragment = new SecondFragment();
+    ThirdFragment thirdFragment = new ThirdFragment();
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.person:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, firstFragment).commit();
+                return true;
+
+            case R.id.home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, secondFragment).commit();
+                return true;
+
+            case R.id.settings:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, thirdFragment).commit();
+                return true;
+        }
+        return false;
     }
 }
